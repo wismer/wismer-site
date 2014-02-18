124ff736c346dc8d8badf5186d7fca4e3501e227
wismer <matthewhl@gmail.com>
2014-02-17 21:22:58 -0500

    works

--- home.rb
+++ home.rb
@@ -1,36 +1,46 @@
 require 'sinatra/base'
 require 'json'
+require 'oauth2'
+require 'net/http'
 # require 'battleship'
 class CrapApp < Sinatra::Base; end
 
 require './log/test'
-
+require './auth'
 class CrapApp
-  get '/users' do 
-    'asd'
+  enable :sessions
+
+  get '/auth' do 
+    client = OAuth2::Client.new(CLIENT, SECRET, :site => 'https://github.com', :authorize_url => '/login/oauth/authorize')
+    redirect client.auth_code.authorize_url(:redirect_uri => 'http://localhost:3000/oauth_callback')
+  end
+
+  get '/oauth_callback' do 
+    access = OAuth2::Client.new(CLIENT, SECRET, :site => 'https://github.com', :authorize_url => '/login/oauth/authorize', :token_url => '/login/oauth/access_token')
+    token  = access.auth_code.get_token(params[:code], :redirect_uri => 'http://localhost:3000/oauth_callback')
+    redirect to("/users?token=#{token.token}")
   end
 
   get '/users/:user' do 
-    data = Crap.new(params[:user])
-    data.repos.each do |rep|
-      "#{rep}"
-      # data.langs(rep) do |cp|
-      #   "#{cp}"
-      # end
-    end
-    # data = JSON.parse(data.show)
-    # data
-    # JSON.parse(data.show)
-    # data = redirect 'https://api.github.com/users/' + params[:user] + '/repos'
-    # JSON.parse(data).to_hash
-    # json = URI('https://api.github.com/' + params[:user] + '/repos')
-    #request.host # ('https://api.github.com/' + params[:user] + '/repos')
-    # fin = json.get(json)
-    # JSON.parse(fin)
+    session[:secret]
+  end
+
+
+  get '/users' do 
+    access = User.new(params[:token])
+    access.show
+  end
+
+  post '/users' do 
 
   end
 
+  link '/' do 
+    '/users'
+  end
+
   get '/battleship' do 
+    TEST
     # battleship stuff here
   end
 
--- log/test.rb
+++ log/test.rb
@@ -1,20 +1,70 @@
 require 'net/https'
-class Crap
-  attr_accessor :user, :name
-  def initialize(user)
-    @user = JSON.parse(Net::HTTP.get(URI("https://api.github.com/users/#{user}/repos")))
-    @name = user
+require 'oauth2'
+require 'json'
+class User 
+  attr_reader :token, :site
+  def initialize(token)
+    @site      = "https://api.github.com"
+    @token     = "?access_token=#{token}"
+    # @token = JSON.parse(Net::HTTP.get(URI("https://api.github.com/user?access_token=#{token}")))
   end
 
-  def langs(repo)
-    yield JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/#{name}/#{repo}/languages")))
+  def users
+    ['wismer', 'clin88', 'plredmond', 'carljv'].map do |name| 
+      lang_data = LangData.new(token, name)
+      lang_data.stats
+        {
+          'user' => { 
+            'name'  => name,
+    # 'langs' => repos, # should return array of repo names
+            'stats' => lang_data.langs # should return a hash of language stats
+          }
+        }
+    end
   end
 
-  def repos
-    user.map { |key| key['name'] }
+  def show
+    users.map do |user|
+      "<ul>#{user}</ul>"
+    end
   end
 end
 
+class LangData 
+  attr_reader :token, :name, :site
+  attr_accessor :langs
+  def initialize(token, name)
+    @token = token
+    @name  = name
+    @langs = {}
+    @site  = "https://api.github.com"
+  end
+
+  def pull_user_repos
+    JSON.parse(Net::HTTP.get(URI(site + "/users/#{name}/repos#{token}"))).map { |p| p['name'] }
+  end
+
+  def stats
+    pull_user_repos.each { |repo| json_data(repo) } if pull_user_repos.size > 0
+  end
+
+  def json_data(repo)
+    JSON.parse(Net::HTTP.get(URI("https://api.github.com/repos/#{name}/#{repo}/languages#{token}"))).each do |lang, val|
+      unless lang.nil? 
+        langs.merge!( { lang => val } ) { |key, old, new_val| old + new_val }
+      end
+    end
+  end
+end
+  # def classify_langs(lang, val)
+  #   case lang
+  #   when 'CoffeeScript' then languages[lang] += amt
+  #   when 'Ruby'         then languages[lang] += amt
+  #   when 'JavaScript'   then languages[lang] += amt
+  #   else
+  #     languages.merge!
+  #   end
+  # end
 # class Tastic < Crap
 
 
